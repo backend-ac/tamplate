b71fb95b45ef035b5f9c6d8df37b9950e2b4105f
ForSureN1 <standin1532@gmail.com>
2025-10-29 14:50:32 +0500

    Update index.blade.php

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -74,7 +74,7 @@
 <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
 <script src="{{ asset('js/jquery.maskedinput.min.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.5/swiper-bundle.min.js?_v=20250416184948"></script>
-<script src="{{ asset('js/index.js') }}"></script>
+<script src="{{ asset('js/index.js?v=1.1') }}"></script>
 </body>
 
 </html>
