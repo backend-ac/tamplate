b1c2d298497dcc73409e3a67676d39226d019fb7
ForSureN1 <standin1532@gmail.com>
2026-01-06 17:41:30 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -53,6 +53,21 @@
               </div>
             </div>
         </section>
+        <section class="about indent">
+          <div class="container">
+            <div class="about__container">
+              <div class="about__left">
+                <h2 class="title">О компании</h2>
+                <p>Компания "Алем Трейд" с 2007 года является надежным поставщиком дизельного топлива оптом. Мы
+                  специализируемся на продаже высококачественного топлива, соответствующего международным стандартам,
+                  что гарантирует его эффективность и безопасность для использования в различных типах двигателей.</p>
+              </div>
+              <div class="about__image">
+                <img src="/storage/blocks/hero/01KC40C49BPDQND5Q2RSN40JXZ.webp" alt="">
+              </div>
+            </div>
+          </div>
+        </section>
         <section class="assortment container indent">
             <h2 class="title">Наш ассортимент топлива</h2>
             <p>Мы предлагаем широкий выбор российского и казахстанского дизельного топлива для различных сфер
