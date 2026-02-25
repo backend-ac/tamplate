94ee7b1789fe967f64a972e320e639cd92b1dda0
ForSureN1 <standin1532@gmail.com>
2026-02-25 14:14:31 +0500

    1

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -57,7 +57,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.5/swiper-bundle.css?_v=20250416184948">
     <link rel="stylesheet" href="{{ asset('css/jquery.fancybox.css') }}">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/styles.css?v=1.14') }}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css?v=1.15') }}">
     
     {{-- Head Metrics --}}
     @if($siteSettings && $siteSettings->head_metrics)
